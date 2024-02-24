2cba2539dc2ae7d3d59d5ff0f2940db58a333e3c
FastFingertips <bugra.coskun@outlook.com>
2024-02-25 01:00:00 +0300

    t+del b

--- letterboxdpy/search.py
+++ letterboxdpy/search.py
@@ -51,9 +51,7 @@ class Search:
     def jsonify(self):
       return json_loads(self.__str__())
 
-
     def get_results(self, end_page: int=MAX_RESULTS_PAGE, max: int=MAX_RESULTS):
-
       data = {
          'available': False,
          'query': self.query,
@@ -65,7 +63,6 @@ class Search:
   
       result_count = 1
       for current_page in range(1, end_page+1):
-
         url = f'{self.url}/page/{current_page}/?adult'
         dom = self.scraper.get_parsed_page(url)
         results = self.get_page_results(dom)
